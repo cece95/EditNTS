72b81cc429ce9b3d1830a71e045397d09a98555b
cece95 <cesaregri@gmail.com>
2020-02-10 11:10:29 +0000

    GPU device defaults to 0 and the vocab_path now defaults to the correct path (based on the project structure)

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def main():
                         default='/home/ml/ydong26/tmp_store/editNTS_%s'%dataset,
                         help='Path to exp storage directory.')
     parser.add_argument('--vocab_path', type=str, dest='vocab_path',
-                        default='../vocab_data/',
+                        default='vocab_data/',
                         help='Path contains vocab, embedding, postag_set')
     parser.add_argument('--load_model', type=str, dest='load_model',
                         default=None,
@@ -195,7 +195,7 @@ def main():
     parser.add_argument('--epochs', type=int, default=50)
     parser.add_argument('--hidden', type=int, default=200)
     parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--device', type=int, default=1,
+    parser.add_argument('--device', type=int, default=0,
                         help='select GPU')
 
     #train_file = '/media/vocab_data/yue/TS/editnet_data/%s/train.df.filtered.pos'%dataset
